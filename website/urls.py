6c3728723a2c240a1e8851a809c60aacba0255ab
qiwiGremL1n <echoes@ex.ua>
2018-02-04 22:59:51 +0200

    add 404.html

--- website/urls.py
+++ website/urls.py
@@ -17,10 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import error404
-
-
-handler404 = error404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- website/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import Context, loader
-
-
-def error404(request):
-    template = loader.get_template('home/404.htm')
-    return HttpResponse(content=template.render(), content_type='text/html; charset=utf-8', status=404)
