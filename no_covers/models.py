10e7b05fe19393280350d210b9f4df90bef710c8
qiwiGremL1n <echoes@ex.ua>
2018-02-10 23:47:30 +0200

    in no_covers.models.Book remove max_length

--- no_covers/models.py
+++ no_covers/models.py
@@ -9,7 +9,7 @@ from django.utils.translation import ugettext_lazy as _
 class Book(models.Model):
     title = models.CharField(_('Title'), max_length=100)
     author = models.CharField(_('Author'), max_length=100)
-    excerpt = models.TextField(_('Excerpt from a book'), max_length=1500)
+    excerpt = models.TextField(_('Excerpt from a book'))
     url_link = models.URLField()
 
     def __str__(self):
