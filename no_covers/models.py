6be242ae96e06bce51ea96b3a8438a5e82e3923e
qiwiGremL1n <echoes@ex.ua>
2018-02-11 14:39:33 +0200

    fix pep8

--- no_covers/models.py
+++ no_covers/models.py
@@ -6,8 +6,8 @@ class Book(models.Model):
     title = models.CharField(_('Title'), max_length=100)
     author = models.CharField(_('Author'), max_length=100)
     excerpt = models.TextField(_('Excerpt from a book'))
-    url_link = models.URLField()
     resource = models.CharField(_('Resource'), max_length=100)
+    url_link = models.URLField()
 
     def __str__(self):
         return self.title
